8d4f7dfbdd18c66a531103c5b5093136bff66204
paultrain <70523554+paultrain@users.noreply.github.com>
2021-09-11 15:54:09 -0300

    Add files via upload

---
+++ Calculadora/calculadora.py
@@ -0,0 +1,24 @@
+import time
+
+print("Hola! Bienvenido a la Calculadora MK II")
+time.sleep(1)
+
+num1 = int(input("Dime el primer numero: "))
+num2 = int(input(f"ok, ahora dime un segundo numero: "))
+
+print(f"Bien, elegiste los numeros: {num1} y {num2}")
+time.sleep(1)
+
+simbolo = input(f"¿Que operacion quieres realizar con estos numeros? (Escribe la primera letra)\n -Sumar\n -Restar\n -Multiplicar\n -Dividir\n")
+
+
+if simbolo == "s" or simbolo == "S":
+    print(f"{num1} + {num2} =", (num1 + num2))
+elif simbolo == "r" or simbolo == "R":
+    print(f"{num1} - {num2} =", (num1 - num2))
+elif simbolo == "m" or simbolo == "M":
+    print(f"{num1} * {num2} =", (num1 * num2))
+elif simbolo == "d" or simbolo == "D":
+    print(f"{num1} / {num2} =", (num1 / num2))
+else:
+    print(f"No has escrito ninguna letra correcta\nS|s para sumar, R|r para restar, M|m para multiplicar o D|d para divir")
