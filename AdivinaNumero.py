e76ed608459302e1f6fbe830ffe89b008e0db178
Pablo <pablomoyano08@gmail.com>
2022-03-15 09:57:00 -0300

    fixed indent

--- AdivinaNumero.py
+++ AdivinaNumero.py
@@ -1,5 +1,6 @@
 import random
 
+
 def adivinar(x):
     random_numero = random.randint(1, x)
     adivina = 0
@@ -10,7 +11,10 @@ def adivinar(x):
         elif adivina > random_numero:
             print("Perdona, adivina de nuevo. Muy alto.")
         else:
-            print(f"Felicidades!!!. Adivinaste el numero {random_numero} correctamente.")
+            print(
+                f"Felicidades!!!. Adivinaste el numero {random_numero}"
+                "correctamente.")
+
 
 def computadora_adivina(x):
     bajo = 1
@@ -20,15 +24,17 @@ def computadora_adivina(x):
         if bajo != alto:
             adivina = random.randint(bajo, alto)
         else:
-            adivina = bajo #tambien podria ser alto.
-        devolucion = input(f"Es {adivina} muy alto (A), muy bajo(B), o correcta(C)?").lower()
+            adivina = bajo  # tambien podria ser alto.
+        devolucion = input(
+            f"Es {adivina} muy alto (A), muy bajo(B), o correcta(C)?").lower()
         if devolucion == 'a':
             alto = adivina - 1
         elif devolucion == 'b':
             bajo = adivina + 1
         else:
-            print(f"Bien! La computadora adivinó tu numero: {adivina} correctamente!!!")
+            print(f"Bien! La computadora adivinó tu numero: {adivina}"
+                  "correctamente!!!")
 
 
-#adivinar(10)
+# adivinar(10)
 computadora_adivina(10)
