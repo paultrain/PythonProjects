5dee32e39e524d119239a2ec0b4f269e26423836
Pablo <pablomoyano08@gmail.com>
2022-03-15 10:06:29 -0300

    fixing

--- Calculadora/calculadora.py
+++ Calculadora/calculadora.py
@@ -4,12 +4,14 @@ print("Hola! Bienvenido a la Calculadora MK II")
 time.sleep(1)
 
 num1 = int(input("Dime el primer numero: "))
-num2 = int(input(f"ok, ahora dime un segundo numero: "))
+num2 = int(input("ok, ahora dime un segundo numero: "))
 
 print(f"Bien, elegiste los numeros: {num1} y {num2}")
 time.sleep(1)
 
-simbolo = input(f"¿Que operacion quieres realizar con estos numeros? (Escribe la primera letra)\n -Sumar\n -Restar\n -Multiplicar\n -Dividir\n")
+simbolo = input(
+    "¿Que operacion quieres realizar con estos numeros? (Escribe la primera"
+    "letra)\n -Sumar\n -Restar\n -Multiplicar\n -Dividir\n")
 
 
 if simbolo == "s" or simbolo == "S":
@@ -21,4 +23,5 @@ elif simbolo == "m" or simbolo == "M":
 elif simbolo == "d" or simbolo == "D":
     print(f"{num1} / {num2} =", (num1 / num2))
 else:
-    print(f"No has escrito ninguna letra correcta\nS|s para sumar, R|r para restar, M|m para multiplicar o D|d para divir")
+    print("No has escrito ninguna letra correcta\nS|s para sumar, R|r para"
+          "restar, M|m para multiplicar o D|d para dividir")
